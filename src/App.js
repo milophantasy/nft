7482e5c9144bb372f3406ddc36c3f5de9a5c7928
milophantasy <milophantasy@gmail.com>
2024-06-16 21:11:07 +0200

    bandeau profil

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
+import bandeau from './assets/Bandeau Profil1400x350x72dpi.jpg'
 import './App.css';
 
+
+
 function App() {
+  const handleClick = () => {
+    console.log('Button was clicked!');
+  };
   return (
     <div className="App">
       <header className="App-header">
+      <img class="bandeau" src={bandeau} className="bandeau" alt="badeau" />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button class="button" onClick={handleClick}>mon bouton</button>
       </header>
     </div>
   );
 }
+
 
 export default App;
